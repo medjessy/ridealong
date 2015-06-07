27ac14dde7cf7668eb87ac778b8f93d555186c67
Jerome Martinez <jerome.e.martinez@gmail.com>
2015-06-07 03:13:09 -0500

    center marker yay

--- client/views/home.js
+++ client/views/home.js
@@ -21,15 +21,15 @@ if (Meteor.isClient) {
     // We can use the `ready` callback to interact with the map API once the map is ready.
     GoogleMaps.ready('smilemap', function(map) {
       // Add a marker to the map once it's ready
-      var marker = new google.maps.Marker({
-        position: map.options.center,
-        map: map.instance
-      });
+      // var marker = new google.maps.Marker({
+      //   position: map.options.center,
+      //   map: map.instance
+      // });
 
       google.maps.event.addListener(map.instance, 'mousemove', function() {
         // var smilemap = map.instance;
         // var center = smilemap.getCenter();
-        console.log("mousemove"); 
+        // console.log("mousemove"); 
       });
 
       google.maps.event.addListener(map.instance, 'dragend', function() {
